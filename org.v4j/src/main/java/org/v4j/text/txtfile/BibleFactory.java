5bf6065fd90ec7a28d38d8b0e98370e8f6c10956
Massimiliano Zattera <b2fjava@gmail.com>
2019-06-22 18:23:46 +0200

    Added support for text files

---
+++ org.v4j/src/main/java/org/v4j/text/txtfile/BibleFactory.java
@@ -0,0 +1,43 @@
+/**
+ * 
+ */
+package org.v4j.text.txtfile;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+import org.v4j.text.alphabet.Alphabet;
+
+/**
+ * Factory class to read different language versions of the Bible. The
+ * documents are read from TXT files in resource folder.
+ * 
+ * @author Massimiliano "Maxi" Zattera
+ *
+ */
+public class BibleFactory {
+
+	/**
+	 * Languages in which the ible is available.
+	 */
+	public static final String[] LANGUAGES = {"Italian", "Latin", "German", "French"};
+	
+	/**
+	 * Name of folder with transcriptions (inside resource folder)
+	 */
+	public static final String TRANSCRIPTION_FOLDER = "Transcriptions\\Bible";
+
+	/**
+	 * Returns a version fo the Bible in given language.
+	 * 
+	 * @return given transcription type for the MZ transcription
+	 * @throws IOException 
+	 * @throws URISyntaxException 
+	 */
+	public static TextFile getDocument(String language) throws IOException, URISyntaxException {
+		URL url = ClassLoader.getSystemResource(TRANSCRIPTION_FOLDER + "/" + language+".txt");
+		return new TextFile(new File(url.toURI()), Alphabet.UTF_16, "UTF-8");
+	}
+}
---
+++ org.v4j/src/main/java/org/v4j/text/txtfile/TextFile.java
@@ -0,0 +1,59 @@
+/**
+ * 
+ */
+package org.v4j.text.txtfile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import org.v4j.text.CompositeText;
+import org.v4j.text.alphabet.Alphabet;
+import org.v4j.util.FileUtil;
+
+/**
+ * This represents a text file. It is composed by line of text.
+ * 
+ * @author Massimiliano "Maxi" Zattera
+ *
+ */
+public class TextFile extends CompositeText<TextLine> {
+
+	private final String id;
+
+	@Override
+	public String getId() {
+		return id;
+	}
+
+	public TextFile(String fileName) throws IOException {
+		this(fileName, Alphabet.UTF_16, "UTF-8");
+	}
+
+	public TextFile(String fileName, String encoding) throws IOException {
+		this(fileName, Alphabet.UTF_16, encoding);
+	}
+
+	public TextFile(String fileName, Alphabet a, String encoding) throws IOException {
+		this(new File(fileName), a, encoding);
+	}
+
+	public TextFile(File file) throws IOException {
+		this(file, Alphabet.UTF_16, "UTF-8");
+	}
+
+	public TextFile(File file, String encoding) throws IOException {
+		this(file, Alphabet.UTF_16, encoding);
+	}
+
+	public TextFile(File file, Alphabet a, String encoding) throws IOException {
+		super(a);
+		id = file.getCanonicalPath();
+		
+		List<String> lines = FileUtil.read(file, encoding);
+
+		for (int i = 0; i < lines.size(); ++i) {
+			addElement(new TextLine(i, lines.get(i)));
+		}
+	}
+}

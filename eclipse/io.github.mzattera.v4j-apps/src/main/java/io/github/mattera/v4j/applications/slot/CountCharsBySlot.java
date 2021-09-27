7dd46ee503fc4c7df4e528da5578dd8f8a9a6783
Massimiliano Zattera <mzattera@gmail.com>
2021-09-27 08:52:04 +0200

    Counts both token and terms now

--- eclipse/io.github.mzattera.v4j-apps/src/main/java/io/github/mattera/v4j/applications/slot/CountCharsBySlot.java
+++ eclipse/io.github.mzattera.v4j-apps/src/main/java/io/github/mattera/v4j/applications/slot/CountCharsBySlot.java
@@ -4,12 +4,15 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import io.github.mattera.v4j.text.ElementFilter;
 import io.github.mattera.v4j.text.alphabet.Alphabet;
 import io.github.mattera.v4j.text.alphabet.SlotAlphabet;
 import io.github.mattera.v4j.text.alphabet.SlotAlphabet.TermDecomposition;
+import io.github.mattera.v4j.text.ivtff.IvtffPage;
 import io.github.mattera.v4j.text.ivtff.IvtffText;
 import io.github.mattera.v4j.text.ivtff.VoynichFactory;
-import io.github.mattera.v4j.text.txt.TextString;
+import io.github.mattera.v4j.text.ivtff.VoynichFactory.Transcription;
+import io.github.mattera.v4j.text.ivtff.VoynichFactory.TranscriptionType;
 import io.github.mattera.v4j.util.Counter;
 
 /**
@@ -19,6 +22,19 @@ import io.github.mattera.v4j.util.Counter;
  *
  */
 public final class CountCharsBySlot {
+
+	/**
+	 * Which transcription to use.
+	 */
+	public static final Transcription TRANSCRIPTION = Transcription.AUGMENTED;
+
+	/**
+	 * Which transcription type to use.
+	 */
+	public static final TranscriptionType TRANSCRIPTION_TYPE = TranscriptionType.CONCORDANCE;
+
+	/** Filter to use on pages before analysis */
+	public static final ElementFilter<IvtffPage> FILTER = null;
 
 	private static final List<String> CHARS = new ArrayList<>();
 	static {
@@ -51,26 +67,23 @@ public final class CountCharsBySlot {
 		CHARS.add("=");
 	}
 
+	private CountCharsBySlot() {
+	}
+
 	public static void main(String[] args) throws Exception {
 
 		// Get the document to process
-		IvtffText doc = VoynichFactory.getDocument(Slots.TRANSCRIPTION, Slots.TRANSCRIPTION_TYPE);
+		IvtffText doc = VoynichFactory.getDocument(TRANSCRIPTION, TRANSCRIPTION_TYPE, Alphabet.SLOT);
 		if (Slots.FILTER != null)
 			doc = doc.filterPages(Slots.FILTER);
 
-		// Replaces text with the "slot" alphabet (tentative)
-		String txt = SlotAlphabet.fromEva(doc.getPlainText());
-
-		// All words after replacement, with no unreadable characters.
-		// NOTE The default alphabet uses only letters as "plain" characters, so don't
-		// replace letters with e.g. numbers or this line won't work.
-		Counter<String> allWords = new TextString(txt, Alphabet.SLOT).getWords(true);
+		Counter<String> allWords = doc.getWords(true);
 
 		// All terms past classification
 		Map<String, TermDecomposition> classified = SlotAlphabet.decompose(allWords.itemSet());
 
-		// Total regular terms (considered)
-		int tot = 0;
+		// Total regular terms and token considered
+		int terms = 0, tokens = 0;
 
 		// Prints statistics
 		System.out.println("Transcription: " + Slots.TRANSCRIPTION + "-" + Slots.TRANSCRIPTION_TYPE
@@ -78,41 +91,63 @@ public final class CountCharsBySlot {
 
 		// Each counters counts occurrence of tokens with a given character in a slot
 		@SuppressWarnings("unchecked")
-		Counter<String>[] slotCounter = new Counter[SlotAlphabet.SLOTS.size()];
-		for (int i = 0; i < slotCounter.length; i++)
-			slotCounter[i] = new Counter<String>();
-
+		Counter<String>[] termSlotCounter = new Counter[SlotAlphabet.SLOTS.size()];
+		Counter<String>[] tokenSlotCounter = new Counter[SlotAlphabet.SLOTS.size()];
+		for (int i = 0; i < termSlotCounter.length; i++) {
+			termSlotCounter[i] = new Counter<String>();
+			tokenSlotCounter[i] = new Counter<String>();
+		}
+		
 		// Count all chars in slots
 		for (TermDecomposition d : classified.values()) {
 			if (d.classification != SlotAlphabet.TermClassification.REGULAR)
 				continue;
 
-			++tot;
+			++terms;
+			tokens += allWords.getCount(d.term);
 			for (int i = 0; i < d.slots1.length; ++i) {
-				if (d.slots1[i].isEmpty())
-					slotCounter[i].count("=");
-				else
-					slotCounter[i].count(d.slots1[i]);
+				if (d.slots1[i].isEmpty()) {
+					termSlotCounter[i].count("=");
+					tokenSlotCounter[i].count("=", allWords.getCount(d.term));
+				}
+				else {
+					termSlotCounter[i].count(d.slots1[i]);
+					tokenSlotCounter[i].count(d.slots1[i], allWords.getCount(d.term));
+				}
 			}
 		}
 
 		// Make sure we show all chars (we do in this way so we preserve the order we
 		// want).
-		for (int i = 0; i < slotCounter.length; ++i) {
-			for (String ch : slotCounter[i].itemSet()) {
+		for (int i = 0; i < termSlotCounter.length; ++i) {
+			for (String ch : termSlotCounter[i].itemSet()) {
 				if (!CHARS.contains(ch))
 					CHARS.add(ch);
 			}
 		}
 
 		// Print results
-		System.out.println("Total terms considered;;;;;" + tot);
+		System.out.println("Total terms considered;;;;;" + terms);
 		System.out.println(";;;;;;;");
 		System.out.println(";Slot 0;Slot 1;Slot 2;Slot 3;Slot 4;Slot 5;Slot 6;Slot 7;Slot 8;Slot 9;Slot 10;Slot 11");
 		for (String ch : CHARS) {
 			System.out.print(ch);
-			for (int i = 0; i < slotCounter.length; i++) {
-				int c = slotCounter[i].getCount(ch);
+			for (int i = 0; i < termSlotCounter.length; i++) {
+				int c = termSlotCounter[i].getCount(ch);
+				System.out.print(";" + (c > 0 ? c + "" : ""));
+			}
+			System.out.println();
+		}
+		System.out.println();
+
+		// Print results
+		System.out.println("Total tokens considered;;;;;" + tokens);
+		System.out.println(";;;;;;;");
+		System.out.println(";Slot 0;Slot 1;Slot 2;Slot 3;Slot 4;Slot 5;Slot 6;Slot 7;Slot 8;Slot 9;Slot 10;Slot 11");
+		for (String ch : CHARS) {
+			System.out.print(ch);
+			for (int i = 0; i < tokenSlotCounter.length; i++) {
+				int c = tokenSlotCounter[i].getCount(ch);
 				System.out.print(";" + (c > 0 ? c + "" : ""));
 			}
 			System.out.println();

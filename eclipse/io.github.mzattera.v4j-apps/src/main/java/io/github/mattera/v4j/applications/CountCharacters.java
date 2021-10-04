45c0ad518f7f8463b637b4c043eb45888714cdc4
Massimiliano Zattera <mzattera@gmail.com>
2021-10-04 09:06:36 +0200

    Improved functionality

--- eclipse/io.github.mzattera.v4j-apps/src/main/java/io/github/mattera/v4j/applications/CountCharacters.java
+++ eclipse/io.github.mzattera.v4j-apps/src/main/java/io/github/mattera/v4j/applications/CountCharacters.java
@@ -3,19 +3,24 @@
  */
 package io.github.mattera.v4j.applications;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
 import java.util.Map.Entry;
 
 import io.github.mattera.v4j.text.ElementFilter;
+import io.github.mattera.v4j.text.Text;
 import io.github.mattera.v4j.text.alphabet.Alphabet;
 import io.github.mattera.v4j.text.ivtff.IvtffPage;
 import io.github.mattera.v4j.text.ivtff.IvtffText;
 import io.github.mattera.v4j.text.ivtff.VoynichFactory;
 import io.github.mattera.v4j.text.ivtff.VoynichFactory.Transcription;
 import io.github.mattera.v4j.text.ivtff.VoynichFactory.TranscriptionType;
+import io.github.mattera.v4j.text.txt.BibleFactory;
+import io.github.mattera.v4j.text.txt.TextFile;
 import io.github.mattera.v4j.util.Counter;
 
 /**
- * Counts the characters in a document.
+ * Counts the characters in the Voynich and the Bibles.
  * 
  * STATUS: Works with a test harness (see CountCharactersTest)
  * 
@@ -32,12 +37,7 @@ public final class CountCharacters {
 	/**
 	 * Which transcription type to use.
 	 */
-	public static final TranscriptionType TRANSCRIPTION_TYPE = TranscriptionType.MAJORITY;
-
-	/**
-	 * Which Alphabet type to use.
-	 */
-	public static final Alphabet ALPHABET = Alphabet.SLOT;
+	public static final TranscriptionType TRANSCRIPTION_TYPE = TranscriptionType.CONCORDANCE;
 
 	/** Filter to use on pages before analysis */
 	public static final ElementFilter<IvtffPage> FILTER = null;
@@ -50,26 +50,60 @@ public final class CountCharacters {
 	 */
 	public static void main(String[] args) {
 		try {
-			// Prints configuration parameters
-			System.out.println("Transcription     : " + TRANSCRIPTION);
-			System.out.println("Transcription Type: " + TRANSCRIPTION_TYPE);
-			System.out.println("Alphabet          : " + ALPHABET);
-			System.out.println("Filter            : " + (FILTER == null ? "<no-filter>" : FILTER));
-			System.out.println();
-
-			IvtffText doc = VoynichFactory.getDocument(TRANSCRIPTION, TRANSCRIPTION_TYPE, ALPHABET);
-			if (FILTER != null)
-				doc = doc.filterPages(FILTER);
-
-			Counter<Character> c = doc.getChars();
-			for (Entry<Character, Integer> e : c.reversed()) {
-				System.out.println(e.getKey() + ";" + e.getValue());
+			for (Alphabet a : Alphabet.getAlphabets()) {
+				processVoynich(a);
+				System.out.println();
 			}
+
+			for (String language : BibleFactory.LANGUAGES) {
+				processModernLanguage(language);
+			System.out.println();
+		}
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
 			System.out.println("\nCompleted.");
 		}
 
+	}
+
+	private static void processVoynich(Alphabet a) throws Exception {
+
+		IvtffText doc = null;
+		try {
+			doc = VoynichFactory.getDocument(TRANSCRIPTION, TRANSCRIPTION_TYPE, a);
+		} catch (IllegalArgumentException e) {
+			// This specific version of the document this unavailable
+			return;
+		}
+		if (FILTER != null)
+			doc = doc.filterPages(FILTER);
+		
+		// Prints configuration parameters
+		System.out.println("Voynich");
+		System.out.println("Transcription     : " + TRANSCRIPTION);
+		System.out.println("Transcription Type: " + TRANSCRIPTION_TYPE);
+		System.out.println("Alphabet          : " + a);
+		System.out.println("Filter            : " + (FILTER == null ? "<no-filter>" : FILTER));
+		System.out.println();
+		
+		Counter<Character> c = doc.getChars();
+		for (Entry<Character, Integer> e : c.reversed()) {
+			System.out.println(e.getKey() + ";" + e.getValue() + ";" + ((double)e.getValue()/c.getTotalCounted()));
+		}		
+		
+	}
+
+	private static void processModernLanguage(String language) throws IOException, URISyntaxException {
+		System.out.println("Bible (" + language + ")");
+
+		TextFile doc = BibleFactory.getDocument(language);
+		Alphabet a = doc.getAlphabet();
+
+		Counter<Character> c = Text.getChars(a.toUpperCase(doc.getPlainText()), a);
+		for (Entry<Character, Integer> e : c.reversed()) {
+			System.out.println(e.getKey() + ";" + e.getValue() + ";" + ((double) e.getValue() / c.getTotalCounted()));
+		}
 	}
 }
